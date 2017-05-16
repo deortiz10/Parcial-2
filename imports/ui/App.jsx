5321e6c68683909b7a3d5080b810d9740f984c67
diego <de.ortiz10@uniandes.edu.co>
2017-05-16 11:02:33 -0500

    first comit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,70 @@
+import React, {Component} from "react";
+import {PropTypes} from "prop-types";
+import { Meteor } from "meteor/meteor";
+import { createContainer} from "meteor/react-meteor-data"
+
+import TweetsResults from "./TweetsResults.jsx";
+import {Tweets} from "../api/Tweets.js";
+import ColombiaMap from "./ColombiaMap"
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.projection;
+
+  }
+
+  changeQuery(evt) {
+    if (evt.key !== "Enter") {
+      return;
+    }
+    // "this" will change in the method call, so I need to save it
+    let component = this;
+
+    console.log(evt.target.value);
+    Meteor.call("twitter.stream", evt.target.value);
+
+  }
+
+getData()
+{
+var data= this.props.tweets;
+
+}
+  render() {
+    console.log("render!");
+    return (
+      <div>
+        <input type="text" onKeyPress={this.changeQuery.bind(this)} placeholder="Query"/>
+        { this.props && this.props.err ?
+          <div>Error: {this.props.err}</div> :
+          <span></span>
+        }
+        <h2>Map</h2>
+        <ColombiaMap
+        width="600"
+         height="600"
+         />
+        <h2>Results:</h2>
+        {this.props && this.props.tweets ?
+          <TweetsResults tweets={this.props.tweets}/> :
+          <p>Enter a query</p>
+        }
+
+      </div>
+
+    );
+  }
+}
+
+App.propTypes = {
+  tweets : PropTypes.array.isRequired
+};
+
+export default AppContainer = createContainer(() => {
+  Meteor.subscribe("tweets");
+
+
+  return {
+    tweets: Tweets.find({}).fetch()
+  };
+}, App);
